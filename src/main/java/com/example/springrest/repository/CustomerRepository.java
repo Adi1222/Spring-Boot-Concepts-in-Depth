a4f1ca135bf64801f66dea3d0fc72c4cdb69495c
Adi1222 <adityachavan677@gmail.com>
2022-08-19 18:43:37 +0530

    Mockito Example

--- src/main/java/com/example/springrest/repository/CustomerRepository.java
+++ src/main/java/com/example/springrest/repository/CustomerRepository.java
@@ -6,9 +6,11 @@ import com.example.springrest.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
     @Query("SELECT new com.example.springrest.dto.CustomerProductMapping(c.name, p.pname) from Customer c join c.products p")
--- src/test/java/com/example/springrest/repository/CustomerRepositoryTest.java
+++ src/test/java/com/example/springrest/repository/CustomerRepositoryTest.java
@@ -1,30 +1,73 @@
 package com.example.springrest.repository;
 
+import com.example.springrest.dto.CustomerProductMapping;
 import com.example.springrest.entities.Customer;
+import com.example.springrest.entities.Product;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
-class CustomerRepositoryTest {
+@ExtendWith(MockitoExtension.class)
+public class CustomerRepositoryTest {
 
-    @Autowired
+    @Mock
     private CustomerRepository customerRepository;
 
+
+    AutoCloseable autoCloseable;
+
+    @BeforeEach
+    void setUp() {
+        /**
+         *  We don't have to write this piece of code as we have used the @ExtendWith(MockitoExtention.class)
+         *   which will open and close the mocks for us
+         */
+        // autoCloseable = MockitoAnnotations.openMocks(this);
+    }
+
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception {
+        //this.autoCloseable.close();
         System.out.println("Finished");
     }
 
+
+
     @Test
+    @Disabled
     void getCustomerProducts() {
     }
 
     @Test
+    @Disabled
     void getProductsOfCustomer() {
     }
 
     @Test
+    @Disabled
     void getCustomerId() {
         //Customer expectedCustomer = new Customer(4, "Barney", "barney@gmail.com", "male", "New York");
 
@@ -38,9 +81,9 @@ class CustomerRepositoryTest {
         String name = "Alex";
 
         // when
-        //boolean exp = customerRepository.checkCustomerByName(name);
+        boolean exp = customerRepository.checkCustomerByName(name);
 
         // then
-        //assertThat(exp).isTrue();
+        assertThat(exp).isTrue();
     }
 }
