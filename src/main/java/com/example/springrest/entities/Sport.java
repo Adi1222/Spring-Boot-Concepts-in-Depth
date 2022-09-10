e2c29d61cdb4406f221558ee05c75dd7460c3383
Adi1222 <adityachavan677@gmail.com>
2022-09-10 16:19:09 +0530

    modification in Sport entity

--- src/main/java/com/example/springrest/entities/Sport.java
+++ src/main/java/com/example/springrest/entities/Sport.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
